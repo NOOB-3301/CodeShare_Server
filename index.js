55dc7a74b14c187758906b87241a954a0a6a7514
NOOB-3301 <sepic466@gmail.com>
2025-04-26 03:13:35 +0530

    Add Socket.IO integration for real-time communication
    - added collabe text editing
    - added show users in room
    - added basic room feature

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
 import express from 'express'
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
 
-
-const app = express()
 
 const port = process.env.PORT || 3000;
 
+const app = express()
 
-app.get("/",async(req,res)=>{
-    res.send("railwayyyyyyyyyyyyyyyyyyy")
+app.get("/",async (req,res) => {
+    res.send("socketttt io")
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+
+const server = createServer(app)
+
+const io = new Server(server)
+// io.origins('*:*');
+
+
+let prevdata=null;
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('join-room', (room)=> {
+        console.log("user entered room", room)
+        socket.join(room)
+        // socket.to(room).emit('user-connected', socket.id)
+        io.in(room).emit('user-connected', socket.id)
+        if (prevdata != null) {
+            socket.emit('code-changed-on-join',prevdata)
+        }
+    })
+
+    socket.on('get-room-users',(room=>{
+        console.log("get room users", room)
+        const users = io.sockets.adapter.rooms.get(room)
+        console.log("users in room", users)
+        socket.emit('room-users', Array.from(users))
+    }))
+
+
+    socket.on('code-changed', (data)=>{
+        prevdata= data.code
+        console.log("code changed", data)
+        socket.to(data.room).emit('code-changed', data.code)
+        console.log(prevdata)
+    })
+
+
+});
+
+server.listen(port, () => {
+    console.log('server running at http://localhost:3000');
+});
